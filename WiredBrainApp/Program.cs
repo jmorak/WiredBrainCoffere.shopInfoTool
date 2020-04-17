5be2cccb4064fe4e829aa93620c2daaaa6e45b56
JMorak <jide.morakinyo@outlook.com>
2020-04-17 09:51:48 +0100

    Added csharp project

---
+++ WiredBrainApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired brain coffee shop!");
+        }
+    }
+}
